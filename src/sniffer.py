469158643576d66692901762bcef9e55e76a56a8
bnthn <you@example.com>
2025-08-01 19:52:53 +0200

    Add asyncio and apply some basic OOP

--- src/sniffer.py
+++ src/sniffer.py
@@ -13,5 +13,5 @@ while True:
     if receiver.rx_code_timestamp != timestamp:
         timestamp = receiver.rx_code_timestamp
         if timestamp != None:
-            print('{ "code": "' + str(receiver.rx_code) + '", "pulselength": "' + str(receiver.rx_pulselength) + '", "protocol": "' + str(receiver.rx_proto) + '" }')
+            print('{ "code": "' + str(receiver.rx_code) + '", "pulselength": "' + str(receiver.rx_pulselength) + '", "protocol": "' + str(receiver.rx_proto) + '", timestamp: "' + str(timestamp) + '" }')
     time.sleep(0.5)
